329d504d78e5d1c6d677f2bc8282f9ab5ca6e2ce
Marylly Araújo Silva <marylly.silva@thoughtworks.com>
2021-04-20 14:07:57 -0300

    Configurado e incluído no CI o passo dos testes unitários

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World do Nosso tutorial');
+  res.status(200).send('Hello World do Nosso tutorial');
 });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.info(`Example app listening at http://localhost:${port}`);
 });
+
+module.exports = app;
