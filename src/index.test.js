4dc83336e7bf1eddb9c7ee0fe46b4a5659a6a95a
Marylly Araújo Silva <marylly.silva@thoughtworks.com>
2021-04-20 15:28:13 -0300

    Correções solicitadas pelo linter

--- src/index.test.js
+++ src/index.test.js
@@ -3,6 +3,10 @@ const app = require('./index');
 
 describe('Test the root path', () => {
   test('It should response the GET method', () => {
-    return request(app).get('/').expect(200);
+    request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      });
   });
 });
