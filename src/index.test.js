70e45da0379691837e517d48af4c09c5f9f7c802
Marylly Araújo Silva <marylly.silva@thoughtworks.com>
2021-04-20 14:32:43 -0300

    Correção da configuração do Jest para o projeto

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,13 @@
-const request = require("supertest");
-const app = require("./index");
+const request = require('supertest');
+const app = require('./index');
 
-describe("Test the root path", () => {
-  test("It should response the GET method", done => {
+describe('Test the root path', () => {
+  test('It should response the GET method', (done) => {
     request(app)
-      .get("/")
-      .then(response => {
+      .get('/')
+      .then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
   });
-})
+});
